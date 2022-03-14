cf7215ad2ccd6b0c7061956cdde7c239a342748b
Jorge Dantas <engjorgedantas@gmail.com>
2022-03-14 20:17:34 -0300

    Create SeasonModel file

--- lib/modules/queens/domain/model/queen_model.dart
+++ lib/modules/queens/domain/model/queen_model.dart
@@ -6,6 +6,7 @@ class QueenModel {
     required this.missCongeniality,
     required this.quote,
     required this.imageUrl,
+    required this.seasonsList,
   });
 
   final int id;
@@ -14,4 +15,23 @@ class QueenModel {
   final bool missCongeniality;
   final String quote;
   final String imageUrl;
+  final List<Seasons> seasonsList;
+  final List<Lipsyncs> lipsyncsList;
 }
+
+class Lipsyncs {
+  Lipsyncs({
+    required this.id,
+    required this.won,
+    required this.name,
+    required this.artist,
+  });
+
+  final int id;
+  final bool won;
+  final String name;
+  final String artist;
+
+
+}
+
---
+++ lib/modules/queens/domain/model/season_model.dart
@@ -0,0 +1,12 @@
+
+class Seasons {
+  Seasons({
+    required this.seasonNumber,
+    required this.id,
+    required this.place,
+  });
+
+  final String seasonNumber;
+  final int id;
+  final int place;
+}
