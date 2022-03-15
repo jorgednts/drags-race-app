f5cdc46bb8acc86cf3361c95256bdaf0467719f9
Jorge Dantas <engjorgedantas@gmail.com>
2022-03-15 10:23:10 -0300

    Create getAllQueens method on DragsRaceRemoteDataSourceImpl

--- lib/modules/queens/external/remote_data_source/drags_race_remote_data_source_impl.dart
+++ lib/modules/queens/external/remote_data_source/drags_race_remote_data_source_impl.dart
@@ -1,7 +1,7 @@
 import 'package:dio/dio.dart';
-import 'package:drags_race_app/modules/queens/data/remote/model/all_queens/all_queens_response.dart';
 
 import '../../constants/queens_constants_url_api.dart';
+import '../../data/mapper/remote_to_model.dart';
 import '../../data/remote/data_source/drags_race_remote_data_source.dart';
 import '../../data/remote/model/all_queens/queen_response.dart';
 import '../../domain/model/all_queens/all_queens_model.dart';
@@ -10,19 +10,19 @@ import '../../domain/model/details/queen_details_model.dart';
 class DragsRaceRemoteDataSourceImpl implements DragsRaceRemoteDataSource {
   DragsRaceRemoteDataSourceImpl({required Dio dio}) : _dio = dio;
 
-  final _dio;
+  final Dio _dio;
 
   @override
   Future<List<AllQueensModel>> getAllQueens() async {
     final response = await _dio.get(QueensConstantsUrlApi.allQueensBaseUrl);
     final queensResponse = QueenResponse.fromJsonList(response.data);
+    final queenModelList = <AllQueensModel>[];
 
     for(final queenResponse in queensResponse){
-
+      final queenModel = convertToAllQueensModelList(queenResponse);
+      queenModelList.add(queenModel);
     }
-
-
-    return;
+    return queenModelList;
   }
 
   @override
