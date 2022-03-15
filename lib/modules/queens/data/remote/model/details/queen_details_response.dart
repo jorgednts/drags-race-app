e9aacefbbda5abc9b6f44b39782ebfd0c3ebfcf3
Jorge Dantas <engjorgedantas@gmail.com>
2022-03-14 23:14:00 -0300

    Generate queen_details_response.g file

--- lib/modules/queens/data/remote/model/details/queen_details_response.dart
+++ lib/modules/queens/data/remote/model/details/queen_details_response.dart
@@ -1,6 +1,33 @@
-import 'package:json_serializable/json_serializable.dart';
+import 'package:json_annotation/json_annotation.dart';
 
+import '../lipsync/lipsyncs_response.dart';
+import '../season/seasons_response.dart';
 
-class QueenResponse{
+part 'queen_details_response.g.dart';
 
+@JsonSerializable(fieldRename: FieldRename.snake)
+class QueenDetailsResponse{
+  QueenDetailsResponse(
+      {this.id,
+        this.name,
+        this.winner,
+        this.missCongeniality,
+        this.imageUrl,
+        this.quote,
+        this.lipsyncs,
+        this.seasons});
+
+  factory QueenDetailsResponse.fromJson(Map<String, dynamic> json) =>
+      _$QueenDetailsResponseFromJson(json);
+
+  final int? id;
+  final String? name;
+  final bool? winner;
+  final bool? missCongeniality;
+  final String? imageUrl;
+  final String? quote;
+  final List<LipsyncsResponse>? lipsyncs;
+  final List<SeasonsResponse>? seasons;
+
+  Map<String, dynamic> toJson() => _$QueenDetailsResponseToJson(this);
 }
---
+++ lib/modules/queens/data/remote/model/details/queen_details_response.g.dart
@@ -0,0 +1,37 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'queen_details_response.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+QueenDetailsResponse _$QueenDetailsResponseFromJson(
+        Map<String, dynamic> json) =>
+    QueenDetailsResponse(
+      id: json['id'] as int?,
+      name: json['name'] as String?,
+      winner: json['winner'] as bool?,
+      missCongeniality: json['miss_congeniality'] as bool?,
+      imageUrl: json['image_url'] as String?,
+      quote: json['quote'] as String?,
+      lipsyncs: (json['lipsyncs'] as List<dynamic>?)
+          ?.map((e) => LipsyncsResponse.fromJson(e as Map<String, dynamic>))
+          .toList(),
+      seasons: (json['seasons'] as List<dynamic>?)
+          ?.map((e) => SeasonsResponse.fromJson(e as Map<String, dynamic>))
+          .toList(),
+    );
+
+Map<String, dynamic> _$QueenDetailsResponseToJson(
+        QueenDetailsResponse instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'name': instance.name,
+      'winner': instance.winner,
+      'miss_congeniality': instance.missCongeniality,
+      'image_url': instance.imageUrl,
+      'quote': instance.quote,
+      'lipsyncs': instance.lipsyncs,
+      'seasons': instance.seasons,
+    };
