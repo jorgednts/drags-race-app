870c7cc9578310fbeb10dedc870f5d82a8007068
Jorge Dantas <engjorgedantas@gmail.com>
2022-03-14 23:42:21 -0300

    Create DragsRaceRemoteDataSource Interface

---
+++ lib/modules/queens/data/remote/data_source/drags_race_remote_data_source.dart
@@ -0,0 +1,10 @@
+import '../../../domain/model/all_queens/all_queens_model.dart';
+import '../../../domain/model/details/queen_details_model.dart';
+
+abstract class DragsRaceRemoteDataSource {
+  Future<List<AllQueensModel>> getAllQueens();
+
+  Future<QueenDetailsModel> getQueen();
+
+  Future<QueenDetailsModel> getTypedQueen();
+}
