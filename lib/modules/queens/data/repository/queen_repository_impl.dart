63fd0f81147308089b5333628a5e8c6875d50279
Jorge Dantas <engjorgedantas@gmail.com>
2022-03-15 19:43:01 -0300

    Create QueenRepositoryImpl

---
+++ lib/modules/queens/data/repository/queen_repository_impl.dart
@@ -0,0 +1,26 @@
+import '../../domain/model/details/queen_details_model.dart';
+import '../../domain/model/queen/queen_model.dart';
+import '../../domain/repository/queen_repository.dart';
+import '../remote/data_source/drags_race_remote_data_source.dart';
+
+class QueenRepositoryImpl implements QueenRepository {
+  QueenRepositoryImpl({
+    required dragsRaceRemoteDataSource,
+  }) : _dragsRaceRemoteDataSource = dragsRaceRemoteDataSource;
+
+  final DragsRaceRemoteDataSource _dragsRaceRemoteDataSource;
+
+  @override
+  Future<List<QueenModel>> getAllQueens() async {
+    final allQueensList = await _dragsRaceRemoteDataSource.getAllQueens();
+    return allQueensList;
+  }
+
+  @override
+  Future<QueenDetailsModel> getQueenByID(int queenID) =>
+      _dragsRaceRemoteDataSource.getQueenByID(queenID);
+
+  @override
+  Future<QueenDetailsModel> getQueenByName(String queenName) =>
+      _dragsRaceRemoteDataSource.getQueenByName(queenName);
+}
