6cfd546eaa8d34217ef3cc1c6dc43125fad222b8
Jorge Dantas <engjorgedantas@gmail.com>
2022-03-17 10:43:26 -0300

    Create Gradient for LoginPage

--- lib/modules/auth/presentation/page/login_page.dart
+++ lib/modules/auth/presentation/page/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:drags_race_app/modules/drag_race_constants/drag_race_constants_colors.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -9,5 +10,20 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   @override
-  Widget build(BuildContext context) => Container();
+  Widget build(BuildContext context) => Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            stops: [0.1, 0.3, 0.7, 1.0],
+            colors: [
+              DragRaceConstantsColors.dragRaceThirdGradientColor,
+              DragRaceConstantsColors.dragRaceSecondGradientColor,
+              DragRaceConstantsColors.dragRaceFirstGradientColor,
+              DragRaceConstantsColors.dragRacePrimaryColor,
+            ],
+          ),
+        ),
+
+      );
 }
--- lib/modules/drag_race_constants/drag_race_constants_colors.dart
+++ lib/modules/drag_race_constants/drag_race_constants_colors.dart
@@ -3,4 +3,7 @@ import 'package:flutter/material.dart';
 class DragRaceConstantsColors{
   static const dragRacePrimaryColor = Color(0xFF78DAF0);
   static const dragRaceSecondaryColor = Color(0xFFF0679C);
+  static const dragRaceFirstGradientColor = Color(0xFF7EE5FC);
+  static const dragRaceSecondGradientColor = Color(0xFF6BC3D6);
+  static const dragRaceThirdGradientColor = Color(0xFF58A0B0);
 }
