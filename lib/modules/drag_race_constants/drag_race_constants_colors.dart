590776d8a725e3f6ccc9dd9fd692f2ebb267232a
Jorge Dantas <engjorgedantas@gmail.com>
2022-03-17 11:13:45 -0300

    Add hintText Constant Color on DragRaceConstantColors

--- lib/modules/drag_race_constants/drag_race_constants_colors.dart
+++ lib/modules/drag_race_constants/drag_race_constants_colors.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 
 class DragRaceConstantsColors{
-  static const dragRacePrimaryColor = Color(0xFF78DAF0);
-  static const dragRaceSecondaryColor = Color(0xFFF0679C);
-  static const dragRaceFirstGradientColor = Color(0xFF7EE5FC);
-  static const dragRaceSecondGradientColor = Color(0xFF6BC3D6);
-  static const dragRaceThirdGradientColor = Color(0xFF58A0B0);
+  static const primaryColor = Color(0xFF78DAF0);
+  static const secondaryColor = Color(0xFFF0679C);
+  static const firstGradientColor = Color(0xFF7EE5FC);
+  static const secondGradientColor = Color(0xFF6BC3D6);
+  static const thirdGradientColor = Color(0xFF58A0B0);
+  static const hintTextColor = Color(0xFF767676);
 }
