aa9b20ba0588dd30fb79735b45f8b2fab53bb632
Jorge Dantas <engjorgedantas@gmail.com>
2022-03-18 10:33:07 -0300

    Organize Imports on QueenListPage

--- lib/modules/queens/presentation/page/queen_list_page.dart
+++ lib/modules/queens/presentation/page/queen_list_page.dart
@@ -1,8 +1,8 @@
-import 'package:drags_race_app/modules/queens/presentation/page/search_queen_page.dart';
 import 'package:flutter/material.dart';
 
-import '../../../../generated/l10n.dart';
 import '../../../drag_race_constants/drag_race_constants_colors.dart';
+import '../../../drag_race_constants/drag_race_constants_strings.dart';
+import 'search_queen_page.dart';
 
 class QueenListPage extends StatefulWidget {
   const QueenListPage({Key? key}) : super(key: key);
@@ -26,9 +26,9 @@ class _QueenListPageState extends State<QueenListPage> {
   Widget build(BuildContext context) => Scaffold(
         appBar: AppBar(
           backgroundColor: DragRaceConstantsColors.primaryColor,
-          title: Text(
-            S.of(context).queenListPageAppBarTitle,
-            style: const TextStyle(fontWeight: FontWeight.bold),
+          title: const Text(
+            DragRaceConstantsStrings.appBarTitle,
+            style: TextStyle(fontWeight: FontWeight.bold),
           ),
           centerTitle: true,
           actions: [
