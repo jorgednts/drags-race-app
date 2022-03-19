38e358d583afac3e00bc3c0b6e7a36e7b6b336ca
Jorge Dantas <engjorgedantas@gmail.com>
2022-03-19 10:40:42 -0300

    Set QueenDetailsPage background

--- lib/modules/queens/presentation/page/queen_details_page.dart
+++ lib/modules/queens/presentation/page/queen_details_page.dart
@@ -1,6 +1,8 @@
+import 'package:drags_race_app/modules/common/drag_race_constants/drag_race_constants_colors.dart';
 import 'package:flutter/material.dart';
 
 import '../../domain/model/details/queen_details_model.dart';
+import '../../../../generated/l10n.dart';
 
 class QueenDetailsPage extends StatefulWidget {
   const QueenDetailsPage({
@@ -16,5 +18,27 @@ class QueenDetailsPage extends StatefulWidget {
 
 class _QueenDetailsPageState extends State<QueenDetailsPage> {
   @override
-  Widget build(BuildContext context) => Container();
+  Widget build(BuildContext context) => Scaffold(
+        extendBodyBehindAppBar: true,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+        body: Container(
+          height: MediaQuery.of(context).size.height,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              stops: [0.1, 0.3, 0.7, 1.0],
+              colors: [
+                DragRaceConstantsColors.thirdGradientColor,
+                DragRaceConstantsColors.secondGradientColor,
+                DragRaceConstantsColors.firstGradientColor,
+                DragRaceConstantsColors.primaryColor,
+              ],
+            ),
+          ),
+        ),
+      );
 }
