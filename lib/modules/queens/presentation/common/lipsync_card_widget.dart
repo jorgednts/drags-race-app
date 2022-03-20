69c27103361ad3fe20b4c31925589799367cddc3
Jorge Dantas <engjorgedantas@gmail.com>
2022-03-20 16:14:13 -0300

    Create LipsyncCardWidget

---
+++ lib/modules/queens/presentation/common/lipsync_card_widget.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+import '../../../../generated/l10n.dart';
+import '../../../common/drag_race_constants/drag_race_constants_colors.dart';
+import '../../domain/model/details/lipsync_model.dart';
+import 'custom_queen_details_text_widget.dart';
+
+class LipsyncCardWidget extends StatelessWidget {
+  const LipsyncCardWidget({
+    required this.lipsync,
+    Key? key,
+  }) : super(key: key);
+
+  final LipsyncModel lipsync;
+  static const shantay = 'Shantay, you stay!';
+  static const sashay = 'Sashay away!';
+
+  @override
+  Widget build(BuildContext context) => Container(
+        height: 130,
+        child: Card(
+          shape: RoundedRectangleBorder(
+            side: const BorderSide(
+              color: DragRaceConstantsColors.secondaryColor,
+              width: 3,
+            ),
+            borderRadius: BorderRadius.circular(25),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.only(top: 15, left: 15),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 15),
+                  child: Row(
+                    children: [
+                      CustomQueenDetailsTextWidget(
+                          text: S.of(context).queenLipsyncInfoSongName),
+                      Text(
+                        lipsync.name,
+                        style: const TextStyle(fontSize: 15),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 15),
+                  child: Row(
+                    children: [
+                      CustomQueenDetailsTextWidget(
+                          text: S.of(context).queenLipsyncInfoSongArtist),
+                      Text(
+                        lipsync.artist,
+                        style: const TextStyle(fontSize: 15),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 15),
+                  child: Row(
+                    children: [
+                      CustomQueenDetailsTextWidget(
+                          text: S.of(context).queenLipsyncInfoResult),
+                      getLipsyncResult(lipsync.won),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+
+  Widget getLipsyncResult(bool lipsyncResult) {
+    if (lipsyncResult == true) {
+      return const Text(
+        shantay,
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 15,
+          color: Colors.greenAccent,
+        ),
+      );
+    } else {
+      return const Text(
+        sashay,
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 15,
+          color: Colors.pinkAccent,
+        ),
+      );
+    }
+  }
+}
--- lib/modules/queens/presentation/common/queen_lipsyncs_info_widget.dart
+++ lib/modules/queens/presentation/common/queen_lipsyncs_info_widget.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 
-import '../../../../generated/l10n.dart';
-import '../../../common/drag_race_constants/drag_race_constants_colors.dart';
 import '../../domain/model/details/lipsync_model.dart';
-import 'custom_queen_details_text_widget.dart';
+import 'lipsync_card_widget.dart';
 
 class QueenLipsyncsInfoWidget extends StatelessWidget {
   const QueenLipsyncsInfoWidget({
@@ -12,91 +10,13 @@ class QueenLipsyncsInfoWidget extends StatelessWidget {
   }) : super(key: key);
 
   final List<LipsyncModel> lipsyncs;
-  static const shantay = 'Shantay, you stay!';
-  static const sashay = 'Sashay away!';
 
   @override
   Widget build(BuildContext context) => ListView.builder(
         physics: const ScrollPhysics(),
         shrinkWrap: true,
         itemCount: lipsyncs.length,
-        itemBuilder: (context, index) => Container(
-          height: 130,
-          child: Card(
-            shape: RoundedRectangleBorder(
-              side: const BorderSide(
-                color: DragRaceConstantsColors.secondaryColor,
-                width: 3,
-              ),
-              borderRadius: BorderRadius.circular(25),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.only(top: 15, left: 15),
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 15),
-                    child: Row(
-                      children: [
-                        CustomQueenDetailsTextWidget(
-                            text: S.of(context).queenLipsyncInfoSongName),
-                        Text(
-                          lipsyncs[index].name,
-                          style: const TextStyle(fontSize: 15),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 15),
-                    child: Row(
-                      children: [
-                        CustomQueenDetailsTextWidget(
-                            text: S.of(context).queenLipsyncInfoSongArtist),
-                        Text(
-                          lipsyncs[index].artist,
-                          style: const TextStyle(fontSize: 15),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 15),
-                    child: Row(
-                      children: [
-                        CustomQueenDetailsTextWidget(
-                            text: S.of(context).queenLipsyncInfoResult),
-                        getLipsyncResult(
-                            lipsyncs[index].won),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
+        itemBuilder: (context, index) =>
+            LipsyncCardWidget(lipsync: lipsyncs[index]),
       );
-
-  Widget getLipsyncResult(bool lipsyncResult) {
-    if (lipsyncResult == true) {
-      return const Text(
-        shantay,
-        style: TextStyle(
-          fontWeight: FontWeight.bold,
-          fontSize: 15,
-          color: Colors.greenAccent,
-        ),
-      );
-    } else {
-      return const Text(
-        sashay,
-        style: TextStyle(
-          fontWeight: FontWeight.bold,
-          fontSize: 15,
-          color: Colors.pinkAccent,
-        ),
-      );
-    }
-  }
 }
