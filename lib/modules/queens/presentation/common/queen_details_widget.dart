1469b71b81610f0b09aaea64dbe1b331d7b26979
Jorge Dantas <engjorgedantas@gmail.com>
2022-03-20 15:55:27 -0300

    Create QueenLipsyncsInfoWidget

--- lib/modules/queens/presentation/common/queen_details_widget.dart
+++ lib/modules/queens/presentation/common/queen_details_widget.dart
@@ -4,6 +4,7 @@ import '../../../../generated/l10n.dart';
 import '../controller/queen_details_controller.dart';
 import 'queen_base_info_widget.dart';
 import 'queen_general_info_widget.dart';
+import 'queen_lipsyncs_info_widget.dart';
 
 class QueenDetailsWidget extends StatelessWidget {
   const QueenDetailsWidget({
@@ -37,11 +38,17 @@ class QueenDetailsWidget extends StatelessWidget {
               Padding(
                 padding: const EdgeInsets.only(left: 20, top: 5, bottom: 5),
                 child: Text(
-                  S.of(context).queenDetailsSeasonsInfo,
+                  S.of(context).queenDetailsLipsyncsInfo,
                   style: const TextStyle(
                       color: Colors.white,
                       fontSize: 25,
                       fontWeight: FontWeight.bold),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 15),
+                child: QueenLipsyncsInfoWidget(
+                  controller: controller,
                 ),
               ),
             ],
