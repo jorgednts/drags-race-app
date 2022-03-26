11265265079f96b10c99294442cc234520cab5a7
Jorge Dantas <engjorgedantas@gmail.com>
2022-03-26 15:48:56 -0300

    Adjust CircularProgressIndicatorWidget padding

--- lib/modules/queens/presentation/common/circular_progress_indicator_widget.dart
+++ lib/modules/queens/presentation/common/circular_progress_indicator_widget.dart
@@ -6,19 +6,17 @@ class CircularProgressIndicatorWidget extends StatelessWidget {
   const CircularProgressIndicatorWidget({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => Center(
-      child: Container(
-        alignment: Alignment.center,
-        height: 80,
-        width: 80,
-        child: const Center(
-          child: Padding(
-            padding: EdgeInsets.only(top: 15),
-            child: CircularProgressIndicator(
-              color: DragRaceConstantsColors.primaryColor,
-            ),
+  Widget build(BuildContext context) => Padding(
+    padding: const EdgeInsets.only(top: 250),
+    child: Center(
+        child: Container(
+          alignment: Alignment.center,
+          height: 80,
+          width: 80,
+          child: const CircularProgressIndicator(
+            color: DragRaceConstantsColors.primaryColor,
           ),
         ),
       ),
-    );
+  );
 }
